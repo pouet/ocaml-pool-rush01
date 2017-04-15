08725784ce18f7c06441fa53b2e887b7c2c14b8c
Jeremy SCHOTTE <jschotte@e2r3p4.42.fr>
2017-04-15 13:58:57 +0200

    main

--- main.ml
+++ main.ml
@@ -1,7 +1,13 @@
+let rec wait_for_escape() =
+    match Sdlevent.wait_event () with
+    | Sdlevent.KEYDOWN { Sdlevent.keysym = Sdlkey.KEY_ESCAPE }      -> print_endline "Bye"
+    | event                                                         -> print_endline (Sdlevent.string_of_event event); wait_for_escape ()
+
 let main () =
         Sdl.init [`VIDEO];
-        Sdlvideo.set_video_mode 200 200 [];
-        Sdltimer.delay 2000;
-        Sdl.quit ()
+        at_exit Sdl.quit;
+        ignore(Sdlvideo.set_video_mode 600 600 []);
+        wait_for_escape ()
 
-let _ = main ()
+let () = 
+    main ()
