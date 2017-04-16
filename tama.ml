fdd7b50cbe171a4154faa3f53fe0ec1c2287cd9f
pouet <nchrupal@student.42.fr>
2017-04-16 11:30:13 +0200

    getters sama
    Signed-off-by: pouet <nchrupal@student.42.fr>

--- tama.ml
+++ tama.ml
@@ -1,45 +1,45 @@
 class tama = 
-    object(self)
+    object (self)
         val mutable health = 100
         val mutable energy = 100
         val mutable hygiene = 100
         val mutable happyness = 100
 
-         method check = health <= 0 || energy <= 0 || hygiene <= 0 || happyness <= 0
+        method check = health <= 0 || energy <= 0 || hygiene <= 0 || happyness <= 0
 
-         method sethealth add =
-             let res = happyness + add in
-             if res > 100
-             then health <- 100
-             else if res < 0
-             then health <- 0
-             else health <- res
+        method sethealth add =
+            let res = happyness + add in
+            if res > 100
+            then health <- 100
+            else if res < 0
+            then health <- 0
+            else health <- res
 
         method setenergy add =
-             let res = happyness + add in
-             if res > 100
-             then energy <- 100
-             else if res < 0
-             then energy <- 0
-             else energy <- res
+            let res = happyness + add in
+            if res > 100
+            then energy <- 100
+            else if res < 0
+            then energy <- 0
+            else energy <- res
 
         method sethygiene add =
-             let res = happyness + add in
-             if res > 100
-             then hygiene <- 100
-             else if res < 0
-             then hygiene <- 0
-             else hygiene <- res
+            let res = happyness + add in
+            if res > 100
+            then hygiene <- 100
+            else if res < 0
+            then hygiene <- 0
+            else hygiene <- res
 
         method sethappyness add =
-             let res = happyness + add in
-             if res > 100
-             then happyness <- 100
-             else if res < 0
-             then happyness <- 0
-             else happyness <- res
+            let res = happyness + add in
+            if res > 100
+            then happyness <- 100
+            else if res < 0
+            then happyness <- 0
+            else happyness <- res
 
-         method eat =
+        method eat =
             self#sethealth 25;
             self#setenergy (-10);
             self#sethygiene (-20);
@@ -61,7 +61,12 @@ class tama =
             self#setenergy (-10);
             self#sethappyness(20)
 
+        method get_health = health
+        method get_energy = energy
+        method get_hygiene = hygiene
+        method get_happyness = happyness
+
         method print_stat =
             Printf.printf "Health:%d Energy:%d Hygiene:%d Happyness:%d\n" health energy hygiene happyness
 
-   end
+    end
